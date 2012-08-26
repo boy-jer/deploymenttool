d8505738f32ae5b426e74e5e12f4d48d7314ba44
Nicolas Feigenbaum <nofuim13@gmail.com>
2012-08-26 16:08:02 -0700

    Rails install with FactoryGirl and rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dst::Application.config.secret_token = '3885c483e0d16d9e391372e2333ff9611946d0540cb62acc2fb8393fb738b3eec75140a8538e4914330376124baf8c8e18bbc5c49f6413b07289ec79f823df96'
