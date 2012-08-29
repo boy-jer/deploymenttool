6f73e304ff442bc997441f01893d1c6a01690449
Nicolas Feigenbaum <nofuim13@gmail.com>
2012-08-29 08:08:19 -0700

    Added some methods to the CampaignMailerHelper and updates some of the CampaignMailer views. Also added a folder to specs in order to test helpers.

--- app/helpers/campaign_mailer_helper.rb
+++ app/helpers/campaign_mailer_helper.rb
@@ -2,4 +2,23 @@ module CampaignMailerHelper
   def get(type)
     
   end
+
+  def message(message)
+    view_folder + '/messages/' + message + '_message'
+  end
+
+  def table(table)
+    view_folder + '/tables/' + table + '_table'
+  end
+
+  def footer(footer)
+    view_folder + '/footers/' + footer + '_footer'
+  end
+
+private
+
+  def view_folder
+    'campaign_mailer'
+  end
+
 end
