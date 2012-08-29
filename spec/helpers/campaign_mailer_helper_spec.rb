2b34dbc7b1aee5b0bad45bb40c63118ce03ad359
Nicolas Feigenbaum <nofuim13@gmail.com>
2012-08-29 08:36:33 -0700

    Added specs for the CampaignMailerHelper.

---
+++ spec/helpers/campaign_mailer_helper_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe CampaignMailerHelper do
+  
+  before :each do 
+    include CampaignMailerHelper
+  end
+  
+  describe '#message' do
+    it 'should return a partial found in the messages folder' do
+      partial_path = message 'test'
+      partial_path.should == 'campaign_mailer/messages/test_message'
+    end
+  end
+
+  describe '#table' do
+    it 'should return a partial found in the tables folder' do
+      partial_path = table 'test'
+      partial_path.should == 'campaign_mailer/tables/test_table'
+    end
+  end
+
+  describe '#footer' do
+    it 'should return a partial found in the footers folder' do
+      partial_path = footer 'test'
+      partial_path.should == 'campaign_mailer/footers/test_footer'
+    end
+  end
+  
+end
