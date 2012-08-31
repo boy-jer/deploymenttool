003ab0529fcc144df7a1208a9bd5254950a7f1db
Nicolas Feigenbaum <nofuim13@gmail.com>
2012-08-30 18:03:55 -0700

    Renamed some specs

--- spec/helpers/campaign_mailer_helper_spec.rb
+++ spec/helpers/campaign_mailer_helper_spec.rb
@@ -7,21 +7,21 @@ describe CampaignMailerHelper do
   end
   
   describe '#message' do
-    it 'should return a partial found in the messages folder' do
+    it 'should return the path to a partial found in the messages folder' do
       partial_path = message 'test'
       partial_path.should == 'campaign_mailer/messages/test_message'
     end
   end
 
   describe '#table' do
-    it 'should return a partial found in the tables folder' do
+    it 'should return the path to a partial found in the tables folder' do
       partial_path = table 'test'
       partial_path.should == 'campaign_mailer/tables/test_table'
     end
   end
 
   describe '#footer' do
-    it 'should return a partial found in the footers folder' do
+    it 'should return the path to a partial found in the footers folder' do
       partial_path = footer 'test'
       partial_path.should == 'campaign_mailer/footers/test_footer'
     end
